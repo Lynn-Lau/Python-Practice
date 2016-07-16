3dacfdeb78ecd4a7bbff3539ed3e109e2ddff563
Lynn <liu.silin@foxmail.com>
2016-07-16 16:14:54 +0800

    Updated the cude

--- coursera code/coursera1.py
+++ coursera code/coursera1.py
@@ -5,13 +5,39 @@ Open a file and read the file line by line.Split the line into words
 and print them.May be some words shoud be split twice.
 
 Author: Lynn-Lau
-Version: 1.0  
+Origin Date: 2016/07/14 Version: 1.0 
+Modified Date: 2016/07/16 Version: 1.1
 '''
 path = 'D:romeo.txt'
+List = []
+sortedList = []
+CountLine = 0
 file = open(path)
+for line in file:
+	Line = line.split()
+	for item in Line:
+		List.append(item)	
+	CountLine = CountLine + 1
+SortedList = List.sort()
+for item in List:
+    if item not in sortedList:
+        sortedList.append(item)
+
+print sortedList
+ 
+#print CountLine
+#print SetList
+'''
 line = file.read().split('\n')
-List0 = line[0].split()
-List1 = line[1].split()
-List2 = line[2].split()
-List = List0 + List1 + List2
+Line0 = line[0].split()
+for item in Line0:
+	List.append(item)
+
 print List
+'''
+'''
+List = line[0].split() + line[1].split() + line[2].split()
+List1 = List.append('who')
+SortedList = List.sort()
+print
+''' 
